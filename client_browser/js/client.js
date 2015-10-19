29df9cf0c3b969dd7984fc8e58139e95a7456c94
K3ig0 <keigo.fic@gmail.com>
2015-10-20 00:55:01 +0200

    Iteración 1 -> Chat básico y Ver usuarios en línea

---
+++ client_browser/js/client.js
@@ -0,0 +1,95 @@
+ //document ready? ->
+$(function() {
+
+//initialize socket.io
+var socket = io();
+
+var userName = '';
+
+$('#join').click(function() {
+	var userName_input = $('#joinOptions .inputUsername').val();
+
+	//alphanumeric usernames [2-15] length
+	if (/^[A-Za-z0-9]{2,15}$/.test(userName_input))
+		socket.emit('login', userName_input);
+	else
+		errorAccessOptions('The username must contain between 2 and 15 alphanumeric characters');
+});
+
+
+socket.on('login', function(data) {
+	//server returns false if the username already exists
+	if (data != false) {
+
+		//this userName is alphanumeric and nobody picked
+		userName = $('#joinOptions .inputUsername').val();
+
+		initChat();
+			
+		//users online
+		for	(i = 0; i < data.length; i++) 
+		    $('#users').append($('<li>').text(data[i]));
+	}
+	else
+		errorAccessOptions('This username already exists');
+});
+
+
+
+//** Functions **/
+function errorAccessOptions(str) {
+	if ($('#joinOptions p:first').html() == null)
+		$('#joinOptions').append($('<p>').text(str));
+	else
+		$('#joinOptions p:first').html(str);	
+}
+
+function initChat() {
+	$('#joinOptions').hide();
+	$('#chat').show();
+
+	//send a message pressing <Enter>
+	$('#inputMsg').bind('enterKey',function(e){
+		var msg = $('#inputMsg').val();
+
+		//check no empty messages
+		if (msg != null && msg != '') {
+
+	   		//send the event 'chat message' to the server
+			socket.emit('chat message', msg);
+
+			//append the message to the list
+			$('#messages').append($('<li>').html('<b>' + userName + ':</b> ' + msg));
+
+			//empty the bar to write new messages
+			$('#inputMsg').val('');
+
+			//scroll automatically to the written message
+			$('#conversation').animate({scrollTop: $('#messages li:last-child').position().top + $('#messages li:last-child').height()});
+		}
+	});
+	$('#inputMsg').keyup(function(e) {
+	    if(e.keyCode == 13)
+	        $(this).trigger('enterKey');
+	});
+
+	//receive chat messages
+	socket.on('chat message', function(msg) {	
+		$('#messages').append($('<li>').html(msg));
+	});
+
+
+	//when a new user enters the chat
+	socket.on('new user', function(userName) {
+		$('#messages').append($('<li>').html('<i>The user <b>' + userName + '</b> is connected</i>'));
+		$('#users').append($('<li>').text(userName));
+	});
+
+
+	//when a user leaves the chat
+	socket.on('user disconnected', function(userName, pos) {
+		$('#messages').append($('<li>').html('<i>The user <b>' + userName + '</b> has left</i>'));
+		$('#users li').eq(pos).remove();
+	});
+}
+});
--- server.js
+++ server.js
@@ -1,15 +1,46 @@
-var express = require('express')
-, app = express()
-, port = 8988;
+var express = require('express'),
+	app = express(),
+	http = require('http').Server(app),
+	io = require('socket.io')(http),
+	port = 8988,
+
+	users = [];
 
 
-// routing
-app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/client_browser/index.html');
-})
+//express.static is a middleware to serve files
+app.use('/', express.static(__dirname + '/client_browser'));
 
-
-// server listening port
-app.listen(port, function () {
+//server listening port
+http.listen(port, function () {
   console.log('Server listening goes on localhost listening at port: %s', port);
+});
+
+
+//handling events
+io.on('connection', function(socket){
+
+	socket.on('login', function(userName){
+		if (users.indexOf(userName) == -1) {
+			users.push(userName);
+			socket.userName = userName;
+			socket.emit('login', users);
+			socket.broadcast.emit('new user', userName);
+		}
+		else
+			socket.emit('login', false); 
+	});
+
+	//when the client emits 'chat message'
+	socket.on('chat message', function(msg){
+		//sends this message to everyone except the client (broadcast)
+		socket.broadcast.emit('chat message', '<b>' + socket.userName + ':</b> ' + msg);
+  	});
+
+	socket.on('disconnect', function(){
+		if (socket.userName != undefined) {
+			var pos = users.indexOf(socket.userName);
+			users.splice(pos, 1);
+			socket.broadcast.emit('user disconnected', socket.userName, pos);
+		}
+	});
 });
