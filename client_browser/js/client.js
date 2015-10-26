611a10affdad4c1d6e2f3ac700a17306d1593747
K3ig0 <keigo.fic@gmail.com>
2015-10-26 22:26:03 +0100

    Iteración 2 -> Las salas.
    Historia 3: Crear/unirse a salas.
    Historia 4: Desconexión de una sala.

--- client_browser/js/client.js
+++ client_browser/js/client.js
@@ -6,59 +6,128 @@ var socket = io();
 
 var userName = '';
 
-//<enter> key on the username textbox to access the chat
-$('#joinOptions .inputUsername').keyup(function(e) {
-    if(e.which == 13) //enter key
-        $('#join').click();
+
+//back button = first screen
+window.onpopstate = function(event) {
+	location.replace(document.URL);
+};
+
+$('#newRoom').click(function() {
+	$('#access').hide();
+	$('#newOptions').show();
 });
-$('#join').click(function() {
-	var userName_input = $('#joinOptions .inputUsername').val();
+
+$('#joinRoom').click(function() {
+	$('#access').hide();
+	$('#joinOptions').show();
+});
+
+
+//<enter> key on a textbox to create a room
+$('#newOptions .inputUsername, #newOptions .inputRoom').keyup(function(e) {
+    if(e.which == 13) //enter key
+        $('#new').click();
+});
+//creating a room
+$('#new').click(function() {
+	var roomName = $('#newOptions .inputRoom').val();
+	userName = $('#newOptions .inputUsername').val();
 
 	//alphanumeric usernames [2-15] length
-	if (/^[A-Za-z0-9]{2,15}$/.test(userName_input))
-		socket.emit('login', userName_input);
-	else
+	if (/^[A-Za-z0-9]{2,15}$/.test(userName)) {
+		if (/^[A-Za-z0-9]{2,10}$/.test(roomName))
+			socket.emit('new room', userName, roomName);
+		else if (roomName == null || roomName == '')
+			socket.emit('new room', userName, null);
+		else {
+			errorAccessOptions('The room name must contain between 2 and 10 alphanumeric characters');
+			return;
+		}
+	} else
 		errorAccessOptions('The username must contain between 2 and 15 alphanumeric characters');
 });
 
-
-socket.on('login', function(data) {
-	//server returns false if the username already exists
-	if (data != false) {
-
-		//this userName is alphanumeric and nobody picked
-		userName = $('#joinOptions .inputUsername').val();
-
-		initChat();
-			
-		//users online
-		for	(i = 0; i < data.length; i++) 
-		    $('#users').append($('<li>').text(data[i]));
+socket.on('created room', function(roomId){
+	//if the roomName is not exists
+	if (roomId != false) {
+		window.history.pushState('create', 'Room: ' + roomId, '/' + roomId);
+		initRoom();
+		$('#roomNumber').append($('<u>').text(roomId));
+		$('#members').append($('<li>').text(userName));
 	}
 	else
+		errorAccessOptions('This room name already exists');
+});
+
+
+//<enter> key on a textbox to join a room
+$('#joinOptions .inputUsername, #joinOptions .inputRoom').keyup(function(e) {
+    if(e.which == 13) //enter key
+        $('#join').click();
+});
+//joining a room
+$('#join').click(function() {
+	var roomName = $('#joinOptions .inputRoom').val();
+	userName = $('#joinOptions .inputUsername').val();
+
+	//alphanumeric usernames [2-15] length
+	if (/^[A-Za-z0-9]{2,15}$/.test(userName)) {
+		if (/^[A-Za-z0-9]{2,10}$/.test(roomName))
+			socket.emit('join room', userName, roomName);
+		else {
+			errorAccessOptions('The room name must contain between 2 and 10 alphanumeric characters');
+			return;
+		}
+	} else
+		errorAccessOptions('The username must contain between 2 and 15 alphanumeric characters');
+});
+
+socket.on('joined room', function(code, members){
+	//if username and roomName are unique -> code = roomId
+	if (code != 0 && code != 1) {
+		history.pushState('join', 'Room: ' + code, '/' + code);
+		initRoom();
+		$('#roomNumber').append($('<u>').text(code));
+		for	(i = 0; i < members.length; i++) 
+	    	$('#members').append($('<li>').text(members[i]));
+	}
+	else if (code == 0)
 		errorAccessOptions('This username already exists');
+	else if (code == 1)
+		errorAccessOptions('Room not found');
+});
+
+
+$('.cancel').click(function() {
+		$('.accessOptions').hide();
+		$('.accessOptions input').val('');
+		$('.accessOptions p').text('');
+		$('#access').show();
 });
 
 
 
 //** Functions **/
 function errorAccessOptions(str) {
-	if ($('#joinOptions p:first').html() == null)
-		$('#joinOptions').append($('<p>').text(str));
+	var parent = '#newOptions';
+	if ($('#joinOptions').css('display') == 'block')
+		parent = '#joinOptions';
+	if ($(parent + ' p:first').html() == null)
+		$(parent).append($('<p>').text(str));
 	else
-		$('#joinOptions p:first').html(str);	
+		$(parent + ' p:first').html(str);	
 }
 
-function initChat() {
-	$('#joinOptions').hide();
+function initRoom() {
+	$('.accessOptions').hide();
 	$('#chat').show();
 
 	//send a message pressing <Enter>
 	$('#inputMsg').bind('enterKey',function(e){
 		var msg = $('#inputMsg').val();
 
-		//check no empty messages
-		if (msg != null && msg != '') {
+		//dont send empty or just spaces messages
+		if (jQuery.trim(msg).length != 0) {
 
 	   		//send the event 'chat message' to the server
 			socket.emit('chat message', msg);
@@ -87,14 +156,14 @@ function initChat() {
 	//when a new user enters the chat
 	socket.on('new user', function(userName) {
 		$('#messages').append($('<li>').html('<i>The user <b>' + userName + '</b> is connected</i>'));
-		$('#users').append($('<li>').text(userName));
+		$('#members').append($('<li>').text(userName));
 	});
 
 
 	//when a user leaves the chat
 	socket.on('user disconnected', function(userName, pos) {
 		$('#messages').append($('<li>').html('<i>The user <b>' + userName + '</b> has left</i>'));
-		$('#users li').eq(pos).remove();
+		$('#members li').eq(pos).remove();
 	});
 }
 });
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 	io = require('socket.io')(http),
 	port = 8988,
 
-	users = [];
+	rooms = [];
 
 
 //express.static is a middleware to serve files
@@ -19,28 +19,116 @@ http.listen(port, function () {
 //handling events
 io.on('connection', function(socket){
 
-	socket.on('login', function(userName){
-		if (users.indexOf(userName) == -1) {
-			users.push(userName);
-			socket.userName = userName;
-			socket.emit('login', users);
-			socket.broadcast.emit('new user', userName);
-		}
+	//*** NEW ROOM ***
+	//when a client emits 'new room'
+	socket.on('new room', function(userName, roomName){
+		var roomId;
+
+		if (roomName != null)
+			roomId = roomName;
 		else
-			socket.emit('login', false); 
+			//generating a room id
+			roomId = randomAlphanumeric(5);
+
+		//if the room id already exists -> generate a new id
+		while (rooms[roomId] != undefined)
+			if (roomName == null)
+				roomId = randomAlphanumeric(5);
+			else {
+				//roomName already exists!
+				socket.emit ('created room', false);
+				return;
+			}
+
+		//store the username in the socket session for this client
+		socket.userName = userName;
+
+		//store the room name in the socket session for this client
+		socket.roomId = roomId;
+
+		//add the room id generated and the username to the global list
+		rooms[roomId] = {members: [userName]};
+
+		//send client to the new room
+		socket.join(roomId);
+
+		//send client his room id
+		socket.emit('created room', roomId);
+	});
+
+	//*** JOIN ROOM ***
+	//when a client emits 'join room'
+	socket.on('join room', function(userName, roomId){
+		//roomName not found
+		if (rooms[roomId] == undefined) {
+			socket.emit('joined room', 1);
+			return;
+		}
+		//userName already exists? unique?
+		else if (findMember(roomId, userName) != -1) {
+			socket.emit('joined room', 0);
+			return;
+		}
+
+		//store the username in the socket session for this client
+		socket.userName = userName;
+
+		//store the room name in the socket session for this client
+		socket.roomId = roomId;
+
+		//add the user to the list of members of the room in the rooms global list
+		rooms[roomId]['members'].push(userName);
+
+		//send client to the new room
+		socket.join(roomId);
+
+		//send client room data
+		socket.emit('joined room', roomId, rooms[roomId]['members']);
+
+		//echo to room's client that a new member has connected
+		socket.broadcast.to(roomId).emit('new user', userName);
 	});
 
 	//when the client emits 'chat message'
 	socket.on('chat message', function(msg){
 		//sends this message to everyone except the client (broadcast)
-		socket.broadcast.emit('chat message', '<b>' + socket.userName + ':</b> ' + msg);
+		socket.broadcast.to(socket.roomId).emit('chat message', '<b>' + socket.userName + ':</b> ' + msg);
   	});
 
 	socket.on('disconnect', function(){
-		if (socket.userName != undefined) {
-			var pos = users.indexOf(socket.userName);
-			users.splice(pos, 1);
-			socket.broadcast.emit('user disconnected', socket.userName, pos);
+		if (socket.roomId != undefined) {
+			var pos = findMember(socket.roomId, socket.userName);
+			//removes the member of the room
+			rooms[socket.roomId]['members'].splice(pos, 1);
+			
+			//if the room has at least one member -> broadcast 'user disconnected'; else remove the room
+			if (rooms[socket.roomId]['members'].length > 0)
+				socket.broadcast.to(socket.roomId).emit('user disconnected', socket.userName, pos);	
+			else
+				delete(rooms[socket.roomId]);
+
+			socket.leave(socket.roomId);
 		}
 	});
 });
+
+
+//generates a random alphanumeric string of 'n' length
+function randomAlphanumeric(n) {
+    var id = '';
+    var domain = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i=0; i < n; i++)
+        id += domain.charAt(Math.floor(Math.random() * domain.length));
+
+    return id;
+}
+
+//find a member of the room X given his userName
+function findMember(roomId, userName) { 
+	for (var i = 0; i < rooms[roomId]['members'].length; i++) {
+		if (rooms[roomId]['members'][i] == userName) 
+			return i;	
+	}
+	return -1;
+}
